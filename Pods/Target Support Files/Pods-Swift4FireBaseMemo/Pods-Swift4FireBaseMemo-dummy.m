cfd6f107b367996f3ff888f0d9ac17fc2e286f93
tatsumi kentaro <tatsuken4290@gmail.com>
2018-02-16 19:27:01 +0900

    firebaseライブラリ追加

---
+++ Pods/Target Support Files/Pods-Swift4FireBaseMemo/Pods-Swift4FireBaseMemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Swift4FireBaseMemo : NSObject
+@end
+@implementation PodsDummy_Pods_Swift4FireBaseMemo
+@end
--- Swift4FireBaseMemo/RegisterViewController.swift
+++ Swift4FireBaseMemo/RegisterViewController.swift
@@ -25,7 +25,7 @@ class RegisterViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
     @IBAction func backButton(_ sender: Any) {
